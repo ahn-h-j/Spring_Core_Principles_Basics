ccab1ee1926eb7006349e1555069dfb86fba5ebb
ahn_h_j <tkgkd246@gmail.com>
2025-02-27 23:25:18 +0900

    fix : Component Scan 스캔 위치 지정

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -6,6 +6,10 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+        //해당 패키지 부터 스캔
+//        basePackages = "hello.core.member",
+        //해당 클래스 부터 스캔
+//        basePackageClasses = AutoAppConfig.class,
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
