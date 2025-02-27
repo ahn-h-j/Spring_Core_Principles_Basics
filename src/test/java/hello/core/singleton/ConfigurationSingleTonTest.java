eb12d78b0d867addf18ff291dae44d01741944e0
ahn_h_j <tkgkd246@gmail.com>
2025-02-27 22:37:22 +0900

    test : SingleTon 테스트

---
+++ src/test/java/hello/core/singleton/ConfigurationSingleTonTest.java
@@ -0,0 +1,29 @@
+package hello.core.singleton;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemberServiceImpl;
+import hello.core.order.OrderServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ConfigurationSingleTonTest {
+
+    @Test
+    void configurationTest(){
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberServiceImpl memberService = applicationContext.getBean("memberService", MemberServiceImpl.class);
+        OrderServiceImpl orderService = applicationContext.getBean("orderService", OrderServiceImpl.class);
+        MemberRepository memberRepository = applicationContext.getBean("memberRepository", MemberRepository.class);
+
+        MemberRepository memberRepository1 = memberService.getMemberRepository();
+        MemberRepository memberRepository2 = orderService.getMemberRepository();
+
+        Assertions.assertThat(memberRepository1).isSameAs(memberRepository);
+        Assertions.assertThat(memberRepository2).isSameAs(memberRepository);
+
+    }
+}
