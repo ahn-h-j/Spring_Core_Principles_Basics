cd541a84d165580c5616bc68d83b5d2e712ecd34
ahn_h_j <tkgkd246@gmail.com>
2025-02-28 23:00:50 +0900

    fix : Provider 기능 추가
    - MyLogger는 request Scope이기에 클라이언트의 request가 있어야함
    - LogDemoController, LogDemoService는 클라이언트 request 전에 MyLogger를 주입받고자 함
    - Provider를 통해 ObjectProvider.getObject()를 호출하는 시점까지 request scope bean의 생성을 지연

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -4,6 +4,7 @@ package hello.core.web;
 import hello.core.common.MyLogger;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,13 +14,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
 
     @RequestMapping("log-demo")
     @ResponseBody
     private String logDemo(HttpServletRequest request){
-        String reqeustURL = request.getRequestURL().toString();
-        myLogger.setRequestUrl(reqeustURL);
+        MyLogger myLogger = myLoggerProvider.getObject();
+        String requestURL = request.getRequestURL().toString();
+        myLogger.setRequestUrl(requestURL);
 
         myLogger.log("controller test");
         logDemoService.logic("testId");
