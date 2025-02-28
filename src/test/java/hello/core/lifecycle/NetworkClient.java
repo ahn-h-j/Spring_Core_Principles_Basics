8298019b3253ad9b79610253dd0bcd23412ec425
ahn_h_j <tkgkd246@gmail.com>
2025-02-28 16:12:25 +0900

    test : Bean을 사용한 Life Cycle 초기화, 종료

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -3,7 +3,7 @@ package hello.core.lifecycle;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient {
     private String url;
 
     public NetworkClient() {
@@ -26,15 +26,13 @@ public class NetworkClient implements InitializingBean, DisposableBean {
         System.out.println("close = " + url);
     }
 
-    @Override
-    public void afterPropertiesSet() throws Exception {
+    public void init(){
         //의존성 주입이 끝나면 호출됨
         connect();
         call("초기화 연결 메세지");
     }
 
-    @Override
-    public void destroy() throws Exception {
+    public void close(){
         disconnect();
     }
 }
