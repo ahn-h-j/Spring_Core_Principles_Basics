a08e32b088965e92f495f12dd3019e53cfb19acf
ahn_h_j <tkgkd246@gmail.com>
2025-02-28 10:47:46 +0900

    fix : Component 충돌 오류로 인해 AppConfig관련 코드 주석 처리

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -3,9 +3,13 @@ package hello.core;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
 
+@Component
 public class MemberApp {
     public static void main(String[] args) {
         //순수 자바 AppConfig 사용
@@ -13,14 +17,15 @@ public class MemberApp {
 //        MemberService memberService = appConfig.memberService();
 
         //스프링 Configuration 사용
-        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
-        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+//        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+//        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+//
+//        Member member = new Member(1L, "memberA", Grade.VIP);
+//        memberService.join(member);
+//
+//        Member findMember = memberService.findMember(1L);
+//        System.out.println("findMember = " + findMember.getName());
+//        System.out.println("member = " + member.getName());
 
-        Member member = new Member(1L, "memberA", Grade.VIP);
-        memberService.join(member);
-
-        Member findMember = memberService.findMember(1L);
-        System.out.println("findMember = " + findMember.getName());
-        System.out.println("member = " + member.getName());
     }
 }
